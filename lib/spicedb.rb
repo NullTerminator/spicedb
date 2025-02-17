a6fe265263405668e8eb140a502c4c43b616b4b3
Chris Marques <test.code.debug@gmail.com>
2025-02-17 13:33:01 -0500

    Generated gem

---
+++ lib/spicedb.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative "spicedb/version"
+
+module Spicedb
+  class Error < StandardError; end
+end
---
+++ lib/spicedb/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Spicedb
+  VERSION = "0.1.0"
+end
